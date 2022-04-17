3b4d3773bd1a8fb2b45d154ac1917c583e9d4e6d
Melody Y <meloyang@amazon.com>
2022-04-17 19:29:50 +1000

    fix create workshop env

--- create-workshop-env.sh
+++ create-workshop-env.sh
@@ -17,18 +17,19 @@ echo "==============================================="
 # Install eksctl on cloud9/cloudshell
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv -v /tmp/eksctl /usr/local/bin
-eksctl version
+echo eksctl version is $(eksctl version)
 
 # Install kubectl on cloud9/cloudshell
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl version --short --client
+echo kubectl version is $(kubectl version --short --client)
 
 # Install helm on cloud9/cloudshell
 wget https://get.helm.sh/helm-v3.6.3-linux-amd64.tar.gz
 tar -xvf helm-v3.6.3-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
-helm version --short
+rm -r linux-amd64
+echo helm cli version is $(helm version --short)
 
 # create S3 bucket for application
 aws s3 mb s3://$S3TEST_BUCKET --region $AWS_REGION
@@ -159,7 +160,7 @@ echo "==============================================="
 echo "  Create EKS Cluster ......"
 echo "==============================================="
 
-cat <<EOF | eksctl create cluster -f -
+eksctl create cluster -f - <<EOF
 ---
 apiVersion: eksctl.io/v1alpha5
 kind: ClusterConfig
