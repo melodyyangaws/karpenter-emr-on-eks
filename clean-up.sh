6730670e377eccec6f004be545e2432fb1c69b60
Melody Y <meloyang@amazon.com>
2022-04-21 00:25:53 +1000

    complete clean up script

--- clean-up.sh
+++ clean-up.sh
@@ -21,6 +21,12 @@ export K_POLICY_ARN=arn:aws:iam::$ACCOUNTID:policy/KarpenterControllerPolicy-${E
 aws iam detach-role-policy --role-name $K_ROLE_NAME --policy-arn $K_POLICY_ARN
 aws iam delete-role --role-name $K_ROLE_NAME
 aws iam delete-policy --policy-arn $K_POLICY_ARN
+# delete Grafana role & policy
+export G_ROLE_NAME=${EMRCLUSTER_NAME}-grafana-prometheus-servicerole
+export G_POLICY_ARN=arn:aws:iam::$ACCOUNTID:policy/${G_ROLE_NAME}-policy
+aws iam detach-role-policy --role-name $G_ROLE_NAME --policy-arn $G_POLICY_ARN
+aws iam delete-role --role-name $G_ROLE_NAME
+aws iam delete-policy --policy-arn $G_POLICY_ARN
 
 # delete EMR virtual cluster & EKS cluster
 export VIRTUAL_CLUSTER_ID=$(aws emr-containers list-virtual-clusters --query "virtualClusters[?name == '${EMRCLUSTER_NAME}' && state == 'RUNNING'].id" --output text)
@@ -30,6 +36,25 @@ aws emr-containers delete-virtual-cluster --id $VIRTUAL_CLUSTER_ID
 export S3TEST_BUCKET=${EMRCLUSTER_NAME}-${ACCOUNTID}-${AWS_REGION}
 aws s3 rm s3://$S3TEST_BUCKET --recursive
 aws s3api delete-bucket --bucket $S3TEST_BUCKET
+
+# delete Grafana workspace
+WID=$(aws grafana list-workspaces --query "workspaces[?name=='$EMRCLUSTER_NAME'].id" --output text)
+if ! [ -z "$WID" ]; then
+	for id in $WID; do
+		sleep 2
+		echo "Delete $id"
+		aws grafana delete-workspace --workspace-id $id
+	done
+fi
+# delete Prometheus worksapce
+PID=$(aws amp list-workspaces --alias $EKSCLUSTER_NAME --query workspaces[].workspaceId --output text)
+if ! [ -z "$PID" ]; then
+	for id in $PID; do
+		sleep 2
+		echo "Delete $id"
+		aws amp delete-workspace --workspace-id $id
+	done
+fi
 
 # delete ALB
 vpcId=$(aws ec2 describe-vpcs --filters Name=tag:"karpenter.sh/discovery",Values=$EKSCLUSTER_NAME --query "Vpcs[*].VpcId" --output text)
@@ -49,7 +74,10 @@ if ! [ -z "$TG" ]; then
 		aws elbv2 delete-target-group --target-group-arn $tg
 	done
 fi
-
+# delete ECR
+export ECR_URL="$ACCOUNTID.dkr.ecr.$AWS_REGION.amazonaws.com"
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URL
+aws ecr delete-repository --repository-name eks-spark-benchmark --force
 # delete rest of CFN
 aws cloudformation delete-stack --stack-name Karpenter-$EKSCLUSTER_NAME
 eksctl delete cluster --name $EKSCLUSTER_NAME
